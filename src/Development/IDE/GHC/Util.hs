a6bbcb477b547bdacb511715668549ee3450280e
Pepe Iborra <pepeiborra@gmail.com>
2020-01-25 13:53:14 +0000

    Rebase multi onto master
    Squash alanz/wip/multi-rebased-az onto master
    
    At the point were it forked from digital-asset/master
    at c74e9b51f131df446c8e011ae79f3370c683f12b
    
    ---
    
    Rules for loading interface files
    
    ---
    
    Typechecking with interface files
    
    ---
    
    Generate .hi and .hie files during type checking
    
    ---
    
    Improve support for hs-boot files
    
    The branch was failing to load GHC because the module graph was missing
    edges between a .hs file and its .hs-boot file. This means the .hs-boot
    file was getting added into the HPT after the .hs file which led to
    confusing errors about variables being out of scope.
    
    The fix is to maintain a map from hs-boot to hs files and then add an
    edge for this case when calling `transitiveDependencies`.
    
    Also tidy up some code in setupEnv which I assume was attempting to fix
    this but in an incorrect manner.
    
    Add the -boot suffix when looking for hi-boot files.
    
    For some reason, the `hi` path is not set to the right thing for
    `hs-boot` files. I don't know why not perhaps it is ok to use an
    existing `.hi` file in place of an `hs-boot` file. More investigation
    needed. My experience is that GHC is quite bad a recompilation avoidance
    for hs-boot files anyway.
    
    For example: https://gitlab.haskell.org/ghc/ghc/issues/17434
    
    Add the -boot suffix when writing interface files
    
    ---
    
    Add a note in the README about the optimal project setup
    
    ---
    
    Refactor GetModIface to not retain TypeChecked module in memory
    
    This improves memory performance on a cold cache.
    
    ---
    
    Trailing whitespace
    
    ---
    
    Turn debug log messages into diagnostics
    
    ---
    
    Add tests for hover/definition on imported symbols
    
    ---
    
    Implement "hie" files for ghc-8.6.5
    
    This means that the .hi files patch can also be used with 8.6.5
    
    ---
    
    hlints
    
    ---
    
    Generate .hie files when missing
    
    ---
    
    Fix subtle bug in setDefaultHieDir
    
    ---
    
    Simplify optimal project setup in README
    
    ---
    
    Move interface loading diagnostics behind --test flag
    
    Reusing the --test flag for this seems harmless, I cannot justify introducing a
    new flag
    
    ---
    
    Avoid expensive interface file generation for files of interest
    
    ---
    
    avoid redundant arguments (thanks Moritz K)
    
    ---
    
    qualify a DAML only comment
    
    ---
    
    Skip module source when generating hie file
    
    thanks Moritz Kiefer for noting that we don't care for the generated .hie files
    to embed module sources
    
    ---
    
    runGhcEnv <-> evalGhcEnv
    
    ---
    
    Apply suggestions from code review
    
    Thanks Moritz Kiefer
    
    Co-Authored-By: Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
    
    ---
    
    Squash mpickering/wip/multi-rebased
    
    Onto its origin on Pepe's branch at
      cafc523a3ef3d85b28cce59d211e2d2182b56de0
    
    ---
    
    Multi component support
    
    Biggest issue currently is that cradle dependencies are not supported
    again
    
    ---
    
    clean up diff
    
    ---
    
    Cleanup and comments
    
    ---
    
    Mask cradleAction
    
    ---
    
    Add show instance for GenerateInterfaceFiles
    
    ---
    
    Use a HashMap rather than list
    
    ---
    
    Pass correct SafeHaskell information to mkIfaceTc
    
    Seems like this was never implemented the first time, woops!
    
    Fixes #424
    
    ---
    
    Track dependencies when using qAddDependentFile
    
    Fixes #492

--- src/Development/IDE/GHC/Util.hs
+++ src/Development/IDE/GHC/Util.hs
@@ -2,71 +2,69 @@
 -- SPDX-License-Identifier: Apache-2.0
 
 -- | General utility functions, mostly focused around GHC operations.
-module Development.IDE.GHC.Util(
+module Development.IDE.GHC.Util
+  (
     -- * HcsEnv and environment
-    HscEnvEq(GhcVersionMismatch, compileTime, runTime), hscEnv, newHscEnvEq,
-    modifyDynFlags,
-    evalGhcEnv,
-    runGhcEnv,
+    HscEnvEq
+  , hscEnv
+  , newHscEnvEq
+  , modifyDynFlags
+  , runGhcEnv
+  , evalGhcEnv
+  , deps
     -- * GHC wrappers
-    prettyPrint,
-    printRdrName,
-    printName,
-    ParseResult(..), runParser,
-    lookupPackageConfig,
-    textToStringBuffer,
-    stringBufferToByteString,
-    moduleImportPath,
-    cgGutsToCoreModule,
-    fingerprintToBS,
-    fingerprintFromStringBuffer,
+  , prettyPrint
+  , printRdrName
+  , printName
+  , ParseResult(..)
+  , runParser
+  , lookupPackageConfig
+  , textToStringBuffer
+  , stringBufferToByteString
+  , moduleImportPath
+  , cgGutsToCoreModule
+  , fingerprintToBS
+  , fingerprintFromStringBuffer
+  ,
     -- * General utilities
-    readFileUtf8,
-    hDuplicateTo',
-    setDefaultHieDir,
-    dontWriteHieFiles
-    ) where
+    readFileUtf8
+  , hDuplicateTo'
+  , setDefaultHieDir
+  , dontWriteHieFiles
+  )
+where
 
-import Control.Concurrent
-import Data.List.Extra
-import Data.ByteString.Internal (ByteString(..))
-import Data.Maybe
-import Data.Typeable
-import qualified Data.ByteString.Internal as BS
-import Fingerprint
-import GhcMonad
-import Control.Exception
-import Data.IORef
-import Data.Version (showVersion, Version)
-import FileCleanup
-import Foreign.Ptr
-import Foreign.ForeignPtr
-import Foreign.Storable
-import GHC.IO.BufferedIO (BufferedIO)
-import GHC.IO.Device as IODevice
-import GHC.IO.Encoding
-import GHC.IO.Exception
-import GHC.IO.Handle.Types
-import GHC.IO.Handle.Internals
-import Data.Unique
-import Development.Shake.Classes
-import qualified Data.Text                as T
-import qualified Data.Text.Encoding       as T
-import qualified Data.Text.Encoding.Error as T
-import qualified Data.ByteString          as BS
-import Lexer
-import StringBuffer
-import System.FilePath
-import HscTypes (cg_binds, md_types, cg_module, ModDetails, CgGuts, ic_dflags, hsc_IC, HscEnv(hsc_dflags))
-import PackageConfig (PackageConfig)
-import Outputable (showSDocUnsafe, ppr, showSDoc, Outputable)
-import Packages (getPackageConfigMap, lookupPackage')
-import SrcLoc (mkRealSrcLoc)
-import FastString (mkFastString)
-import DynFlags (emptyFilesToClean, unsafeGlobalDynFlags)
-import Module (moduleNameSlashes)
-import OccName (parenSymOcc)
-import RdrName (nameRdrName, rdrNameOcc)
+import           Control.Concurrent
+import           Data.List.Extra
+import           Data.ByteString.Internal       ( ByteString(..) )
+import           Data.Maybe
+import           Data.Typeable
+import qualified Data.ByteString.Internal      as BS
+import           Data.Version (showVersion, Version)
+import           Fingerprint
+import           GhcMonad
+import           GhcPlugins              hiding ( Unique, (<>) )
+import           Data.IORef
+import           Control.Exception
+import           FileCleanup
+import           Foreign.Ptr
+import           Foreign.ForeignPtr
+import           Foreign.Storable
+import           GHC.IO.BufferedIO              ( BufferedIO )
+import           GHC.IO.Device                 as IODevice
+import           GHC.IO.Encoding
+import           GHC.IO.Exception
+import           GHC.IO.Handle.Types
+import           GHC.IO.Handle.Internals
+import           Data.Unique
+import           Development.Shake.Classes
+import qualified Data.Text                     as T
+import qualified Data.Text.Encoding            as T
+import qualified Data.Text.Encoding.Error      as T
+import qualified Data.ByteString               as BS
+import           Lexer
+import           StringBuffer
+import           System.FilePath
 
 import Development.IDE.GHC.Compat as GHC
 import Development.IDE.Types.Location
@@ -83,17 +81,16 @@ modifyDynFlags f = do
   -- We do not use setSessionDynFlags here since we handle package
   -- initialization separately.
   modifySession $ \h ->
-    h { hsc_dflags = newFlags, hsc_IC = (hsc_IC h) {ic_dflags = newFlags} }
+    h { hsc_dflags = newFlags, hsc_IC = (hsc_IC h) { ic_dflags = newFlags } }
 
 -- | Given a 'UnitId' try and find the associated 'PackageConfig' in the environment.
 lookupPackageConfig :: UnitId -> HscEnv -> Maybe PackageConfig
-lookupPackageConfig unitId env =
-    lookupPackage' False pkgConfigMap unitId
-    where
-        pkgConfigMap =
-            -- For some weird reason, the GHC API does not provide a way to get the PackageConfigMap
-            -- from PackageState so we have to wrap it in DynFlags first.
-            getPackageConfigMap $ hsc_dflags env
+lookupPackageConfig unitId env = lookupPackage' False pkgConfigMap unitId
+ where
+  pkgConfigMap =
+      -- For some weird reason, the GHC API does not provide a way to get the PackageConfigMap
+      -- from PackageState so we have to wrap it in DynFlags first.
+    getPackageConfigMap $ hsc_dflags env
 
 
 -- | Convert from the @text@ package to the @GHC@ 'StringBuffer'.
@@ -103,14 +100,14 @@ textToStringBuffer = stringToStringBuffer . T.unpack
 
 runParser :: DynFlags -> String -> P a -> ParseResult a
 runParser flags str parser = unP parser parseState
-    where
-      filename = "<interactive>"
-      location = mkRealSrcLoc (mkFastString filename) 1 1
-      buffer = stringToStringBuffer str
-      parseState = mkPState flags buffer location
+ where
+  filename   = "<interactive>"
+  location   = mkRealSrcLoc (mkFastString filename) 1 1
+  buffer     = stringToStringBuffer str
+  parseState = mkPState flags buffer location
 
 stringBufferToByteString :: StringBuffer -> ByteString
-stringBufferToByteString StringBuffer{..} = PS buf cur len
+stringBufferToByteString StringBuffer {..} = PS buf cur len
 
 -- | Pretty print a GHC value using 'unsafeGlobalDynFlags '.
 prettyPrint :: Outputable a => a -> String
@@ -119,8 +116,7 @@ prettyPrint = showSDoc unsafeGlobalDynFlags . ppr
 -- | Pretty print a 'RdrName' wrapping operators in parens
 printRdrName :: RdrName -> String
 printRdrName name = showSDocUnsafe $ parenSymOcc rn (ppr rn)
-  where
-    rn = rdrNameOcc name
+  where rn = rdrNameOcc name
 
 -- | Pretty print a 'Name' wrapping operators in parens
 printName :: Name -> String
@@ -168,6 +164,7 @@ moduleImportPath (takeDirectory . fromNormalizedFilePath -> pathDir) pm
 --   if they are created with the same call to 'newHscEnvEq'.
 data HscEnvEq
     = HscEnvEq !Unique !HscEnv
+               [(InstalledUnitId, DynFlags)] -- In memory components for this HscEnv
     | GhcVersionMismatch { compileTime :: !Version
                          , runTime     :: !(Maybe Version)
                          }
@@ -175,9 +172,10 @@ data HscEnvEq
 -- | Unwrap an 'HsEnvEq'.
 hscEnv :: HscEnvEq -> HscEnv
 hscEnv = either error id . hscEnv'
+-- hscEnv (HscEnvEq _ x _) = x
 
 hscEnv' :: HscEnvEq -> Either String HscEnv
-hscEnv' (HscEnvEq _ x) = Right x
+hscEnv' (HscEnvEq _ x _) = Right x
 hscEnv' GhcVersionMismatch{..} = Left $
     unwords
         ["ghcide compiled against GHC"
@@ -187,25 +185,29 @@ hscEnv' GhcVersionMismatch{..} = Left $
         ,". This is unsupported, ghcide must be compiled with the same GHC version as the project."
         ]
 
+deps :: HscEnvEq -> [(InstalledUnitId, DynFlags)]
+deps (HscEnvEq _ _ u) = u
+deps (GhcVersionMismatch _ _) = []
+
 -- | Wrap an 'HscEnv' into an 'HscEnvEq'.
-newHscEnvEq :: HscEnv -> IO HscEnvEq
-newHscEnvEq e = do u <- newUnique; return $ HscEnvEq u e
+newHscEnvEq :: HscEnv -> [(InstalledUnitId, DynFlags)] -> IO HscEnvEq
+newHscEnvEq e uids = do u <- newUnique; return $ HscEnvEq u e uids
 
 instance Show HscEnvEq where
-  show (HscEnvEq a _) = "HscEnvEq " ++ show (hashUnique a)
+  show (HscEnvEq a _ _) = "HscEnvEq " ++ show (hashUnique a)
   show GhcVersionMismatch{..} = "GhcVersionMismatch " <> show (compileTime, runTime)
 
 instance Eq HscEnvEq where
-  HscEnvEq a _ == HscEnvEq b _ = a == b
+  HscEnvEq a _ _ == HscEnvEq b _ _ = a == b
   GhcVersionMismatch a b == GhcVersionMismatch c d = a == c && b == d
   _ == _ = False
 
 instance NFData HscEnvEq where
-  rnf (HscEnvEq a b) = rnf (hashUnique a) `seq` b `seq` ()
+  rnf (HscEnvEq a b c) = rnf (hashUnique a) `seq` b `seq` c `seq` ()
   rnf GhcVersionMismatch{} = rnf runTime
 
 instance Hashable HscEnvEq where
-  hashWithSalt salt (HscEnvEq u _) = hashWithSalt salt u
+  hashWithSalt salt (HscEnvEq u _ _) = hashWithSalt salt u
   hashWithSalt salt GhcVersionMismatch{..} = hashWithSalt salt (compileTime, runTime)
 
 -- Fake instance needed to persuade Shake to accept this type as a key.
@@ -220,90 +222,99 @@ readFileUtf8 f = T.decodeUtf8With T.lenientDecode <$> BS.readFile f
 
 -- | Convert from a 'CgGuts' to a 'CoreModule'.
 cgGutsToCoreModule :: SafeHaskellMode -> CgGuts -> ModDetails -> CoreModule
-cgGutsToCoreModule safeMode guts modDetails = CoreModule
-    (cg_module guts)
-    (md_types modDetails)
-    (cg_binds guts)
-    safeMode
+cgGutsToCoreModule safeMode guts modDetails =
+  CoreModule (cg_module guts) (md_types modDetails) (cg_binds guts) safeMode
 
 -- | Convert a 'Fingerprint' to a 'ByteString' by copying the byte across.
 --   Will produce an 8 byte unreadable ByteString.
 fingerprintToBS :: Fingerprint -> BS.ByteString
 fingerprintToBS (Fingerprint a b) = BS.unsafeCreate 8 $ \ptr -> do
-    ptr <- pure $ castPtr ptr
-    pokeElemOff ptr 0 a
-    pokeElemOff ptr 1 b
+  ptr <- pure $ castPtr ptr
+  pokeElemOff ptr 0 a
+  pokeElemOff ptr 1 b
 
 -- | Take the 'Fingerprint' of a 'StringBuffer'.
 fingerprintFromStringBuffer :: StringBuffer -> IO Fingerprint
 fingerprintFromStringBuffer (StringBuffer buf len cur) =
-    withForeignPtr buf $ \ptr -> fingerprintData (ptr `plusPtr` cur) len
+  withForeignPtr buf $ \ptr -> fingerprintData (ptr `plusPtr` cur) len
 
 
 -- | A slightly modified version of 'hDuplicateTo' from GHC.
 --   Importantly, it avoids the bug listed in https://gitlab.haskell.org/ghc/ghc/merge_requests/2318.
 hDuplicateTo' :: Handle -> Handle -> IO ()
-hDuplicateTo' h1@(FileHandle path m1) h2@(FileHandle _ m2)  = do
- withHandle__' "hDuplicateTo" h2 m2 $ \h2_ -> do
-   -- The implementation in base has this call to hClose_help.
-   -- _ <- hClose_help h2_
-   -- hClose_help does two things:
-   -- 1. It flushes the buffer, we replicate this here
-   _ <- flushWriteBuffer h2_ `catch` \(_ :: IOException) -> pure ()
-   -- 2. It closes the handle. This is redundant since dup2 takes care of that
-   -- but even worse it is actively harmful! Once the handle has been closed
-   -- another thread is free to reallocate it. This leads to dup2 failing with EBUSY
-   -- if it happens just in the right moment.
-   withHandle_' "hDuplicateTo" h1 m1 $ \h1_ -> do
-     dupHandleTo path h1 Nothing h2_ h1_ (Just handleFinalizer)
-hDuplicateTo' h1@(DuplexHandle path r1 w1) h2@(DuplexHandle _ r2 w2)  = do
- withHandle__' "hDuplicateTo" h2 w2  $ \w2_ -> do
-   _ <- hClose_help w2_
-   withHandle_' "hDuplicateTo" h1 w1 $ \w1_ -> do
-     dupHandleTo path h1 Nothing w2_ w1_ (Just handleFinalizer)
- withHandle__' "hDuplicateTo" h2 r2  $ \r2_ -> do
-   _ <- hClose_help r2_
-   withHandle_' "hDuplicateTo" h1 r1 $ \r1_ -> do
-     dupHandleTo path h1 (Just w1) r2_ r1_ Nothing
-hDuplicateTo' h1 _ =
-  ioe_dupHandlesNotCompatible h1
+hDuplicateTo' h1@(FileHandle path m1) h2@(FileHandle _ m2) = do
+  withHandle__' "hDuplicateTo" h2 m2 $ \h2_ -> do
+    -- The implementation in base has this call to hClose_help.
+    -- _ <- hClose_help h2_
+    -- hClose_help does two things:
+    -- 1. It flushes the buffer, we replicate this here
+    _ <- flushWriteBuffer h2_ `catch` \(_ :: IOException) -> pure ()
+    -- 2. It closes the handle. This is redundant since dup2 takes care of that
+    -- but even worse it is actively harmful! Once the handle has been closed
+    -- another thread is free to reallocate it. This leads to dup2 failing with EBUSY
+    -- if it happens just in the right moment.
+    withHandle_' "hDuplicateTo" h1 m1 $ \h1_ -> do
+      dupHandleTo path h1 Nothing h2_ h1_ (Just handleFinalizer)
+hDuplicateTo' h1@(DuplexHandle path r1 w1) h2@(DuplexHandle _ r2 w2) = do
+  withHandle__' "hDuplicateTo" h2 w2 $ \w2_ -> do
+    _ <- hClose_help w2_
+    withHandle_' "hDuplicateTo" h1 w1 $ \w1_ -> do
+      dupHandleTo path h1 Nothing w2_ w1_ (Just handleFinalizer)
+  withHandle__' "hDuplicateTo" h2 r2 $ \r2_ -> do
+    _ <- hClose_help r2_
+    withHandle_' "hDuplicateTo" h1 r1 $ \r1_ -> do
+      dupHandleTo path h1 (Just w1) r2_ r1_ Nothing
+hDuplicateTo' h1 _ = ioe_dupHandlesNotCompatible h1
 
 -- | This is copied unmodified from GHC since it is not exposed.
-dupHandleTo :: FilePath
-            -> Handle
-            -> Maybe (MVar Handle__)
-            -> Handle__
-            -> Handle__
-            -> Maybe HandleFinalizer
-            -> IO Handle__
-dupHandleTo filepath h other_side
-            _hto_@Handle__{haDevice=devTo}
-            h_@Handle__{haDevice=dev} mb_finalizer = do
-  flushBuffer h_
-  case cast devTo of
-    Nothing   -> ioe_dupHandlesNotCompatible h
-    Just dev' -> do
-      _ <- IODevice.dup2 dev dev'
-      FileHandle _ m <- dupHandle_ dev' filepath other_side h_ mb_finalizer
-      takeMVar m
+dupHandleTo
+  :: FilePath
+  -> Handle
+  -> Maybe (MVar Handle__)
+  -> Handle__
+  -> Handle__
+  -> Maybe HandleFinalizer
+  -> IO Handle__
+dupHandleTo filepath h other_side _hto_@Handle__ { haDevice = devTo } h_@Handle__ { haDevice = dev } mb_finalizer
+  = do
+    flushBuffer h_
+    case cast devTo of
+      Nothing   -> ioe_dupHandlesNotCompatible h
+      Just dev' -> do
+        _              <- IODevice.dup2 dev dev'
+        FileHandle _ m <- dupHandle_ dev' filepath other_side h_ mb_finalizer
+        takeMVar m
 
 -- | This is copied unmodified from GHC since it is not exposed.
 -- Note the beautiful inline comment!
-dupHandle_ :: (IODevice dev, BufferedIO dev, Typeable dev) => dev
-           -> FilePath
-           -> Maybe (MVar Handle__)
-           -> Handle__
-           -> Maybe HandleFinalizer
-           -> IO Handle
-dupHandle_ new_dev filepath other_side _h_@Handle__{..} mb_finalizer = do
+dupHandle_
+  :: (IODevice dev, BufferedIO dev, Typeable dev)
+  => dev
+  -> FilePath
+  -> Maybe (MVar Handle__)
+  -> Handle__
+  -> Maybe HandleFinalizer
+  -> IO Handle
+dupHandle_ new_dev filepath other_side _h_@Handle__ {..} mb_finalizer = do
    -- XXX wrong!
-  mb_codec <- if isJust haEncoder then fmap Just getLocaleEncoding else return Nothing
-  mkHandle new_dev filepath haType True{-buffered-} mb_codec
-      NewlineMode { inputNL = haInputNL, outputNL = haOutputNL }
-      mb_finalizer other_side
+  mb_codec <- if isJust haEncoder
+    then fmap Just getLocaleEncoding
+    else return Nothing
+  mkHandle new_dev
+           filepath
+           haType
+           True{-buffered-}mb_codec
+           NewlineMode { inputNL = haInputNL, outputNL = haOutputNL }
+           mb_finalizer
+           other_side
 
 -- | This is copied unmodified from GHC since it is not exposed.
 ioe_dupHandlesNotCompatible :: Handle -> IO a
-ioe_dupHandlesNotCompatible h =
-   ioException (IOError (Just h) IllegalOperation "hDuplicateTo"
-                "handles are incompatible" Nothing Nothing)
+ioe_dupHandlesNotCompatible h = ioException
+  (IOError (Just h)
+           IllegalOperation
+           "hDuplicateTo"
+           "handles are incompatible"
+           Nothing
+           Nothing
+  )
