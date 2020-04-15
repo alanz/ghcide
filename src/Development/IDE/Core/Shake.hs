fcbe0ae9369f37e4ae2877a4e924c9ba5900c1b1
Alan Zimmerman <alan.zimm@gmail.com>
2020-04-15 23:32:47 +0100

    Report progress as a percentage too.
    This allows it to work with https://github.com/emacs-lsp/lsp-mode/pull/1586

--- src/Development/IDE/Core/Shake.hs
+++ src/Development/IDE/Core/Shake.hs
@@ -350,7 +350,7 @@ lspShakeProgress getLspId sendMsg inProgress = do
                   { _title = "Processing"
                   , _cancellable = Nothing
                   , _message = Nothing
-                  , _percentage = Nothing
+                  , _percentage = Just (0.0)
                   }
                 }
         stop id = sendMsg $ LSP.NotWorkDoneProgressEnd $ LSP.fmServerWorkDoneProgressEndNotification
@@ -374,7 +374,7 @@ lspShakeProgress getLspId sendMsg inProgress = do
                         , _value = LSP.WorkDoneProgressReportParams
                         { _cancellable = Nothing
                         , _message = next
-                        , _percentage = Nothing
+                        , _percentage = Just ((fromIntegral done) / (fromIntegral todo))
                         }
                         }
             loop id next
